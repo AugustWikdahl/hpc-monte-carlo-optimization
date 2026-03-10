be82efaa4e6df7e00d57e1102eb270232880970f
AugustWikdahl <159790745+AugustWikdahl@users.noreply.github.com>
2026-03-10 09:18:02 +0100

    Introduced Prange to numba version
    Co-authored-by: mackan754 <mackan754@users.noreply.github.com>

--- numba_version/main.py
+++ numba_version/main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numba import jit
+from numba import jit, prange
 import math
 
 @jit(nopython=True, fastmath=True)
@@ -7,7 +7,7 @@ def step_math(S, drift, vol):
     z = np.random.standard_normal()
     return S * math.exp(drift + vol * z)
 
-@jit(nopython=True, fastmath=True)
+@jit(nopython=True, fastmath=True, parallel=True)
 def mc_price_numba(S0, K, r, sigma, T, M, I):
     dt = T / M
     drift = (r - 0.5 * sigma**2) * dt
@@ -15,7 +15,7 @@ def mc_price_numba(S0, K, r, sigma, T, M, I):
     
     total_payoff = 0.0
     
-    for i in range(I):
+    for i in prange(I):
         S = S0
         for t in range(M):
             S = step_math(S, drift, vol)
